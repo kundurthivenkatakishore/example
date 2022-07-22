a39c2bc8185ee7a2ae1bdd55327e9dff1979b535
kundurthivenkatakishore <kundurthivenkatakishore@gmail.com>
2022-07-22 17:09:47 +0530

    intial commit

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Main() {
+    const[data,setData]=useState({});
+
+    const getPokemenData=async ()=>{
+        const responce=await fetch("https://pokeapi.co/api/v2/pokemon/")
+        const jsonData=await responce.json();
+        console.log(jsonData)
+        setData(jsonData);
+    }
+
+    useEffect(()=>{
+        getPokemenData();
+    })
+  return (
+    <div>
+        <h1>Pokeman Names</h1>
+        <li>{data.count}</li>
+    </div>
+  )
+}
